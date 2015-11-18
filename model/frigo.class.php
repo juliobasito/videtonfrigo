82e4bb7114d6f6a9b7923de680a5cca2a45bd3d2
louischarbonier <louis.charbonier@y-nov.com>
2015-11-18 11:28:26 +0100

    frigo class

--- model/frigo.class.php
+++ model/frigo.class.php
@@ -1,11 +1,13 @@
 <?php
-
-
-
 class Frigo{
 
 	public static function getIngredientsFrigo($userId)
 	{
+		include('bdd.php');
+		$sql = $db->prepare("SELECT * FROM ingredient i JOIN frigo f ON i.IngredientId = f.IngredientId WHERE f.UserId = :userId");
+		$flag = array('userId' =>$userId);
+		$sql->execute($flag);
+		echo json_encode($sql);
 
 	}
 	public static function getIngredientsFrigo($userId, $filtre)
