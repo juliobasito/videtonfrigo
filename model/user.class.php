d6cffdbfc3941741d784691ac45848d1ac2ddc4e
louischarbonier <louis.charbonier@y-nov.com>
2015-11-18 09:55:31 +0100

    test

--- model/user.class.php
+++ model/user.class.php
@@ -7,16 +7,18 @@ class User{
 	public static function addUser($pseudo, $email, $password, $birthdate, $city, $budget)
 	{
 		include('bdd.php');
-		
+		$sql =$db->prepare("INSERT INTO user (Pseudo, Email, Password, BirthDate, City, Budget) VALUES (:pseudo, :email, :password, :birthdate, :city, :budget)");
+		$flag = array('pseudo' => $pseudo ,'email' => $email, 'password' => $password, 'birthdate' => $birthdate, 'city' => $city, 'budget' => $budger);
+		$sql = $sql->execute($flag);
 	}
 
 	public static function connexion($pseudo, $password)
 	{
 		include('bdd.php');
-		$sql ="SELECT * FROM user WHERE password = '".$password."' AND Pseudo = '".$pseudo."'";
-		$result = $db->prepare($sql);
-		$columns = $result->execute();
-		$columns = $result->fetch();
+		$sql = $db->prepare("SELECT * FROM user WHERE password = :password AND Pseudo = :pseudo");
+		$flag = array('password' => $password , 'pseudo' => $pseudo );
+		$columns = $sql->execute($flag);
+		$columns = $columns->fetch();
 		if ( sizeof($columns) > 1)
 
 		{
